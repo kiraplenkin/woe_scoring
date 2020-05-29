1ceaa920d9d734ee72bb381aec11088eb39c75f9
kiraplenkin <kiraplenkin@gmail.com>
2020-05-29 17:42:06 +0300

    Black formating

--- scorecard/core/functions.py
+++ scorecard/core/functions.py
@@ -3,82 +3,73 @@ import warnings
 import numpy as np
 from scipy.stats import chisquare
 from typing import Dict, List, Tuple
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 
 
-def _merge_bins_for_min_pcnt(X: np.ndarray,
-                             y: np.ndarray,
-                             bad_rates: Dict,
-                             bins: List,
-                             cat: bool = False) -> Tuple[Dict, List, float]:
+def _merge_bins_for_min_pcnt(
+    X: np.ndarray, y: np.ndarray, bad_rates: Dict, bins: List, cat: bool = False
+) -> Tuple[Dict, List, float]:
 
-    min_idx = np.argmin([bad_rate['pcnt'] for bad_rate in bad_rates])
+    min_idx = np.argmin([bad_rate["pcnt"] for bad_rate in bad_rates])
 
     if min_idx == 0:
-        bins[min_idx+1] += bins[min_idx]
-    elif min_idx == len(bad_rates)-1:
-        bins[min_idx-1] += bins[min_idx]
+        bins[min_idx + 1] += bins[min_idx]
+    elif min_idx == len(bad_rates) - 1:
+        bins[min_idx - 1] += bins[min_idx]
     else:
         if cat:
-            if np.abs(bad_rates[min_idx]['bad_rate'] - bad_rates[min_idx-1]['bad_rate']) \
-                < np.abs(bad_rates[min_idx]['bad_rate'] - bad_rates[min_idx+1]['bad_rate']):
-                bins[min_idx-1] += bins[min_idx]
+            if np.abs(
+                bad_rates[min_idx]["bad_rate"] - bad_rates[min_idx - 1]["bad_rate"]
+            ) < np.abs(
+                bad_rates[min_idx]["bad_rate"] - bad_rates[min_idx + 1]["bad_rate"]
+            ):
+                bins[min_idx - 1] += bins[min_idx]
             else:
-                bins[min_idx+1] += bins[min_idx]
+                bins[min_idx + 1] += bins[min_idx]
         else:
             temp_bins = copy.deepcopy(bins)
-            temp_bins[min_idx] += temp_bins[min_idx-1]
-            del temp_bins[min_idx-1]
-            temp_bad_rates, temp_bins, overall_rate = bin_bad_rate(X=X,
-                                                                   y=y,
-                                                                   bins=temp_bins,
-                                                                   cat=False)
-            chi_1 = _chi2(bad_rates=temp_bad_rates,
-                          overall_rate=overall_rate)
+            temp_bins[min_idx] += temp_bins[min_idx - 1]
+            del temp_bins[min_idx - 1]
+            temp_bad_rates, temp_bins, overall_rate = bin_bad_rate(
+                X=X, y=y, bins=temp_bins, cat=False
+            )
+            chi_1 = _chi2(bad_rates=temp_bad_rates, overall_rate=overall_rate)
             del temp_bins
 
             temp_bins = copy.deepcopy(bins)
-            temp_bins[min_idx] += temp_bins[min_idx+1]
-            del temp_bins[min_idx+1]
-            temp_bad_rates, temp_bins, overall_rate = bin_bad_rate(X=X,
-                                                                   y=y,
-                                                                   bins=temp_bins,
-                                                                   cat=False)
-            chi_2 = _chi2(bad_rates=temp_bad_rates,
-                          overall_rate=overall_rate)
+            temp_bins[min_idx] += temp_bins[min_idx + 1]
+            del temp_bins[min_idx + 1]
+            temp_bad_rates, temp_bins, overall_rate = bin_bad_rate(
+                X=X, y=y, bins=temp_bins, cat=False
+            )
+            chi_2 = _chi2(bad_rates=temp_bad_rates, overall_rate=overall_rate)
             del temp_bins
 
             if chi_1 < chi_2:
-                bins[min_idx-1] += bins[min_idx]
+                bins[min_idx - 1] += bins[min_idx]
             else:
-                bins[min_idx+1] += bins[min_idx]
+                bins[min_idx + 1] += bins[min_idx]
 
     del bins[min_idx]
-    bad_rates, bins, overall_rate = bin_bad_rate(X=X,
-                                                 y=y,
-                                                 bins=bins,
-                                                 cat=True)
+    bad_rates, bins, overall_rate = bin_bad_rate(X=X, y=y, bins=bins, cat=True)
     return (bad_rates, bins, overall_rate)
 
 
-def _chi2(bad_rates: Dict,
-          overall_rate: float) -> float:
+def _chi2(bad_rates: Dict, overall_rate: float) -> float:
 
-    f_obs = [bin['bad'] for bin in bad_rates]
-    f_exp = [bin['total'] * overall_rate for bin in bad_rates]
+    f_obs = [bin["bad"] for bin in bad_rates]
+    f_exp = [bin["total"] * overall_rate for bin in bad_rates]
 
-    chi2 = chisquare(f_obs=f_obs,
-                     f_exp=f_exp)[0]
-    
+    chi2 = chisquare(f_obs=f_obs, f_exp=f_exp)[0]
+
     return chi2
 
 
-def bin_bad_rate(X: np.ndarray,
-                 y: np.ndarray,
-                 bins: List,
-                 mask: str = 'NaN',
-                 cat: bool = False) -> Tuple[Dict, List, float]:
-    
+def bin_bad_rate(
+    X: np.ndarray, y: np.ndarray, bins: List, mask: str = "NaN", cat: bool = False
+) -> Tuple[Dict, List, float]:
+
     bad_rates = []
     for value in bins:
         if cat:
@@ -95,22 +86,26 @@ def bin_bad_rate(X: np.ndarray,
                 woe = np.log(good_pcnt / 0.000001)
             iv = (good_pcnt - bad_pcnt) * woe
             stats = {
-                'bin': value,
-                'total': total,
-                'bad': bad,
-                'pcnt': pcnt,
-                'bad_rate': bad_rate,
-                'woe': woe,
-                'iv': iv
+                "bin": value,
+                "total": total,
+                "bad": bad,
+                "pcnt": pcnt,
+                "bad_rate": bad_rate,
+                "woe": woe,
+                "iv": iv,
             }
         else:
             X_not_na = X[~np.isin(X, mask)]
             y_not_na = y[~np.isin(X, mask)]
-            X_isin = X_not_na[np.where((X_not_na >= np.min(value)) & (X_not_na <= np.max(value)))]
+            X_isin = X_not_na[
+                np.where((X_not_na >= np.min(value)) & (X_not_na <= np.max(value)))
+            ]
             total = len(X_isin)
             bad = y_not_na[np.isin(X_not_na, X_isin)].sum()
             pcnt = np.sum(np.isin(X_not_na, X_isin)) * 1.0 / len(X)
-            bad_rate = y_not_na[np.isin(X_not_na, X_isin)].sum() / len(y_not_na[np.isin(X_not_na, X_isin)])
+            bad_rate = y_not_na[np.isin(X_not_na, X_isin)].sum() / len(
+                y_not_na[np.isin(X_not_na, X_isin)]
+            )
             good = total - bad
             good_pcnt = good / total
             bad_pcnt = bad / total
@@ -120,65 +115,58 @@ def bin_bad_rate(X: np.ndarray,
                 woe = np.log(good_pcnt / 0.000001)
             iv = (good_pcnt - bad_pcnt) * woe
             stats = {
-                'bin': value,
-                'total': total,
-                'bad': bad,
-                'pcnt': pcnt,
-                'bad_rate': bad_rate,
-                'woe': woe,
-                'iv': iv
+                "bin": value,
+                "total": total,
+                "bad": bad,
+                "pcnt": pcnt,
+                "bad_rate": bad_rate,
+                "woe": woe,
+                "iv": iv,
             }
         bad_rates.append(stats)
-        
+
     if cat:
-        bad_rates.sort(key=lambda x: x['bad_rate'])
-    
+        bad_rates.sort(key=lambda x: x["bad_rate"])
+
     N, B = 0, 0
     for bin in bad_rates:
-        N += bin['total']
-        B += bin['bad']
-    
+        N += bin["total"]
+        B += bin["bad"]
+
     overall_rate = B * 1.0 / N
-    
-    return (bad_rates, [bin['bin'] for bin in bad_rates], overall_rate)
+
+    return (bad_rates, [bin["bin"] for bin in bad_rates], overall_rate)
 
 
-def cat_bining(X: np.ndarray,
-               y: np.ndarray,
-               min_pcnt_group: float,
-               n_finale: int,
-               temperature: float,
-               mask: str) -> Dict:
+def cat_bining(
+    X: np.ndarray,
+    y: np.ndarray,
+    min_pcnt_group: float,
+    n_finale: int,
+    temperature: float,
+    mask: str,
+) -> Dict:
 
     bins = list([bin] for bin in np.unique(X))
-    bad_rates, bins, _ = bin_bad_rate(X=X,
-                                      y=y,
-                                      bins=bins,
-                                      cat=True)
-    
+    bad_rates, bins, _ = bin_bad_rate(X=X, y=y, bins=bins, cat=True)
+
     if len(bins) <= 2:
         return bad_rates
     else:
         # null to worst bin
         for i, bin in enumerate(bins):
             if bin[0] == mask:
-                if (i != len(bins)-1 and len(bins) > 2):
-                    bins[len(bins)-1] += bins[i]
+                if i != len(bins) - 1 and len(bins) > 2:
+                    bins[len(bins) - 1] += bins[i]
                     del bins[i]
-                    bad_rates, bins, _ = bin_bad_rate(X=X,
-                                                      y=y,
-                                                      bins=bins,
-                                                      cat=True)
+                    bad_rates, bins, _ = bin_bad_rate(X=X, y=y, bins=bins, cat=True)
 
         # 0 bad_rate group to nearest bin
-        while (bad_rates[0]['bad_rate'] == 0 and len(bad_rates) > 2):
+        while bad_rates[0]["bad_rate"] == 0 and len(bad_rates) > 2:
             bins[0] += bins[1]
             del bins[1]
-            bad_rates, bins, _ = bin_bad_rate(X=X,
-                                              y=y,
-                                              bins=bins,
-                                              cat=True)
-        
+            bad_rates, bins, _ = bin_bad_rate(X=X, y=y, bins=bins, cat=True)
+
         # proportion from best bin and small pcnt on values
         # prop = 0
         # while (prop < temperature and len(bad_rates) > 2):
@@ -196,47 +184,45 @@ def cat_bining(X: np.ndarray,
         #                                               bins=bins,
         #                                               cat=True)
         #             break
-        
+
         # min_pcnt_group threshold to nearest bad_rate group
-        while min([bad_rate['pcnt'] for bad_rate in bad_rates]) < min_pcnt_group:
-            bad_rates, bins, _ = _merge_bins_for_min_pcnt(X=X,
-                                                          y=y,
-                                                          bad_rates=bad_rates,
-                                                          bins=bins,
-                                                          cat=True)
-        
+        while min([bad_rate["pcnt"] for bad_rate in bad_rates]) < min_pcnt_group:
+            bad_rates, bins, _ = _merge_bins_for_min_pcnt(
+                X=X, y=y, bad_rates=bad_rates, bins=bins, cat=True
+            )
+
         # count of bins > max_group
         while len(bad_rates) > n_finale:
-            bad_rates, bins, _ = _merge_bins_for_min_pcnt(X=X,
-                                                          y=y,
-                                                          bad_rates=bad_rates,
-                                                          bins=bins,
-                                                          cat=True)
-        
+            bad_rates, bins, _ = _merge_bins_for_min_pcnt(
+                X=X, y=y, bad_rates=bad_rates, bins=bins, cat=True
+            )
+
         return bad_rates
 
 
-def num_bining(X: np.ndarray,
-               y: np.ndarray,
-               min_pcnt_group: float,
-               n_finale: int,
-               max_bins: int,
-               mask: str) -> Dict:
+def num_bining(
+    X: np.ndarray,
+    y: np.ndarray,
+    min_pcnt_group: float,
+    n_finale: int,
+    max_bins: int,
+    mask: str,
+) -> Dict:
 
     if len(np.unique(X[~np.isin(X, mask)])) > max_bins:
         N = len(np.unique(X[~np.isin(X, mask)]))
         n = N // max_bins
         split_point_index = [i * n for i in range(1, max_bins)]
-        bins = list([bin] for bin in np.unique([X[~np.isin(X, mask)][i] for i in split_point_index]))
+        bins = list(
+            [bin]
+            for bin in np.unique([X[~np.isin(X, mask)][i] for i in split_point_index])
+        )
     else:
         bins = list([bin] for bin in np.unique(X[~np.isin(X, mask)]))
 
     bins[0] = list([np.NINF, bins[0][0]])
-    bins[len(bins)-1] = list([bins[len(bins)-1][0], np.inf])
-    bad_rates, bins, overall_rate = bin_bad_rate(X=X,
-                                                 y=y,
-                                                 bins=bins,
-                                                 cat=False)
+    bins[len(bins) - 1] = list([bins[len(bins) - 1][0], np.inf])
+    bad_rates, bins, overall_rate = bin_bad_rate(X=X, y=y, bins=bins, cat=False)
 
     if len(bins) <= 2:
         return bad_rates
@@ -245,53 +231,52 @@ def num_bining(X: np.ndarray,
             chiq_list = []
             for i in range(len(bad_rates) - 1):
                 temp_bins = copy.deepcopy(bins)
-                temp_bins[i] += temp_bins[i+1]
-                del temp_bins[i+1]
-                temp_bad_rates, temp_bins, overall_rate = bin_bad_rate(X=X,
-                                                                       y=y,
-                                                                       bins=temp_bins,
-                                                                       cat=False)
-                chiq_list.append(_chi2(bad_rates=temp_bad_rates,
-                                       overall_rate=overall_rate))
+                temp_bins[i] += temp_bins[i + 1]
+                del temp_bins[i + 1]
+                temp_bad_rates, temp_bins, overall_rate = bin_bad_rate(
+                    X=X, y=y, bins=temp_bins, cat=False
+                )
+                chiq_list.append(
+                    _chi2(bad_rates=temp_bad_rates, overall_rate=overall_rate)
+                )
                 del temp_bins
             best_combined = chiq_list.index(min(chiq_list))
-            bins[best_combined] += bins[best_combined+1]
-            del bins[best_combined+1]
-            bad_rates, bins, overall_rate = bin_bad_rate(X=X,
-                                                         y=y,
-                                                         bins=bins,
-                                                         cat=False)
+            bins[best_combined] += bins[best_combined + 1]
+            del bins[best_combined + 1]
+            bad_rates, bins, overall_rate = bin_bad_rate(X=X, y=y, bins=bins, cat=False)
 
         #  TODO add preparing missing values
 
         if min_pcnt_group > 0:
-            while min([bad_rate['pcnt'] for bad_rate in bad_rates]) < min_pcnt_group:
-                bad_rates, bins, overall_rate = _merge_bins_for_min_pcnt(X=X,
-                                                                         y=y,
-                                                                         bad_rates=bad_rates,
-                                                                         bins=bins,
-                                                                         cat=False)
+            while min([bad_rate["pcnt"] for bad_rate in bad_rates]) < min_pcnt_group:
+                bad_rates, bins, overall_rate = _merge_bins_for_min_pcnt(
+                    X=X, y=y, bad_rates=bad_rates, bins=bins, cat=False
+                )
 
         while not _bad_rate_monotone(bad_rates) and len(bad_rates) > 2:
-            bad_rates, bins, overall_rate = _merge_bins_for_min_pcnt(X=X,
-                                                                     y=y,
-                                                                     bad_rates=bad_rates,
-                                                                     bins=bins,
-                                                                     cat=False)
+            bad_rates, bins, overall_rate = _merge_bins_for_min_pcnt(
+                X=X, y=y, bad_rates=bad_rates, bins=bins, cat=False
+            )
 
         return bad_rates
 
 
 def _bad_rate_monotone(bad_rates: Dict) -> bool:
-    
+
     if len(bad_rates) <= 2:
         return True
-    
-    bad_rate_not_monotone = [(bad_rates[i]['bad_rate'] < bad_rates[i+1]['bad_rate']
-                             and bad_rates[i]['bad_rate'] < bad_rates[i-1]['bad_rate'])
-                             or (bad_rates[i]['bad_rate'] > bad_rates[i+1]['bad_rate']
-                             and bad_rates[i]['bad_rate'] > bad_rates[i-1]['bad_rate'])
-                             for i in range(1, len(bad_rates)-1)]
+
+    bad_rate_not_monotone = [
+        (
+            bad_rates[i]["bad_rate"] < bad_rates[i + 1]["bad_rate"]
+            and bad_rates[i]["bad_rate"] < bad_rates[i - 1]["bad_rate"]
+        )
+        or (
+            bad_rates[i]["bad_rate"] > bad_rates[i + 1]["bad_rate"]
+            and bad_rates[i]["bad_rate"] > bad_rates[i - 1]["bad_rate"]
+        )
+        for i in range(1, len(bad_rates) - 1)
+    ]
 
     if bad_rate_not_monotone:
         return False
--- scorecard/core/main.py
+++ scorecard/core/main.py
@@ -10,16 +10,18 @@ from .functions import num_bining
 
 
 class WOETransformer(BaseEstimator, TransformerMixin):
-    def __init__(self,
-                 n_finale: int = 10,
-                 max_bins: int = 100,
-                 min_pcnt_group: float = 0.05,
-                 verbose: bool = False,
-                 cat_features: List = None,
-                 cat_features_threshold: int = 0,
-                 missing_mask: str = 'NaN',
-                 cat_features_temperature: float = 0.005,
-                 specials: List = []):
+    def __init__(
+        self,
+        n_finale: int = 10,
+        max_bins: int = 100,
+        min_pcnt_group: float = 0.05,
+        verbose: bool = False,
+        cat_features: List = None,
+        cat_features_threshold: int = 0,
+        missing_mask: str = "NaN",
+        cat_features_temperature: float = 0.005,
+        specials: List = [],
+    ):
         """
         Performs the Weight Of Evidence transformation over the input X features using information from y vector.
         :param verbose: boolean flag to add verbose output
@@ -38,11 +40,9 @@ class WOETransformer(BaseEstimator, TransformerMixin):
 
         self.WOE_IV_dict = []  # self.transformers
         self.feature_names = []
-        self.num_features  = []
+        self.num_features = []
 
-    def fit(self,
-            X: Union[pd.DataFrame, np.ndarray],
-            y: Union[pd.Series, np.ndarray]):
+    def fit(self, X: Union[pd.DataFrame, np.ndarray], y: Union[pd.Series, np.ndarray]):
         """
         Fits the input data
         :param X: data matrix
@@ -55,54 +55,70 @@ class WOETransformer(BaseEstimator, TransformerMixin):
             else:
                 self.feature_names = X.columns
         elif isinstance(X, np.ndarray):
-            self.feature_names = ['X%i' for i in range(X.shape[-1])]
+            self.feature_names = ["X%i" for i in range(X.shape[-1])]
         else:
-            raise TypeError('X vector is not np array neither data frame')
+            raise TypeError("X vector is not np array neither data frame")
 
         X, y = self._check_inputs(X, y)
-        
+
         if len(self.cat_features) == 0 and self.cat_features_threshold > 0:
             for i in range(len(self.feature_names)):
-                if type(X[0, i]) == np.dtype('object') \
-                    or type(X[0, i]) == np.dtype('str') \
-                    or len(np.unique(X[:, i][~np.isin(X[:, i], self.missing_mask)])) < self.cat_features_threshold:
+                if (
+                    type(X[0, i]) == np.dtype("object")
+                    or type(X[0, i]) == np.dtype("str")
+                    or len(np.unique(X[:, i][~np.isin(X[:, i], self.missing_mask)]))
+                    < self.cat_features_threshold
+                ):
                     self.cat_features.append(self.feature_names[i])
         if len(self.cat_features) > 0:
-            self.num_features = [feature for feature in self.feature_names if feature not in self.cat_features]
+            self.num_features = [
+                feature
+                for feature in self.feature_names
+                if feature not in self.cat_features
+            ]
             for feature in self.cat_features:
                 feature_idx = list(self.feature_names).index(feature)
-                self._print(f'Preparing {feature} feature')
+                self._print(f"Preparing {feature} feature")
                 self.WOE_IV_dict.append(
-                    {feature: cat_bining(X=X[:, feature_idx],
-                                         y=y,
-                                         min_pcnt_group=self.min_pcnt_group,
-                                         n_finale=self.n_finale,
-                                         temperature=self.cat_features_temperature,
-                                         mask=self.missing_mask)}
+                    {
+                        feature: cat_bining(
+                            X=X[:, feature_idx],
+                            y=y,
+                            min_pcnt_group=self.min_pcnt_group,
+                            n_finale=self.n_finale,
+                            temperature=self.cat_features_temperature,
+                            mask=self.missing_mask,
+                        )
+                    }
                 )
         else:
             self.num_features = self.feature_names
 
         for feature in self.num_features:
             feature_idx = list(self.feature_names).index(feature)
-            self._print(f'Preparing {feature} feature')
+            self._print(f"Preparing {feature} feature")
             self.WOE_IV_dict.append(
-                {feature: num_bining(X=X[:, feature_idx],
-                                     y=y,
-                                     min_pcnt_group=self.min_pcnt_group,
-                                     n_finale=self.n_finale,
-                                     max_bins=self.max_bins,
-                                     mask=self.missing_mask)}
+                {
+                    feature: num_bining(
+                        X=X[:, feature_idx],
+                        y=y,
+                        min_pcnt_group=self.min_pcnt_group,
+                        n_finale=self.n_finale,
+                        max_bins=self.max_bins,
+                        mask=self.missing_mask,
+                    )
+                }
             )
-    
+
         return self
 
-
-    def transform(self,
-                  X: Union[pd.DataFrame, np.ndarray],
-                  y: Union[pd.Series, np.ndarray] = None,
-                  hight_IV_threshold: float = 0.02,
-                  suffix: str = '_WOE'):
+    def transform(
+        self,
+        X: Union[pd.DataFrame, np.ndarray],
+        y: Union[pd.Series, np.ndarray] = None,
+        hight_IV_threshold: float = 0.02,
+        suffix: str = "_WOE",
+    ):
         """
         Checks and transforms input arrays
         :param X: X data array
@@ -115,60 +131,66 @@ class WOETransformer(BaseEstimator, TransformerMixin):
                 for feature in dict.keys():
                     sum_iv = 0
                     for iv in dict[feature]:
-                        sum_iv += iv['iv']
+                        sum_iv += iv["iv"]
                     if sum_iv > hight_IV_threshold:
                         self.feature_names.append(feature)
-        
+
         for feature in self.feature_names:
             new_feature = feature + suffix
             for feature_dict in self.WOE_IV_dict:
                 if list(feature_dict)[0] == feature:
                     for bin in feature_dict[feature]:
                         if feature in self.cat_features:
-                            X.loc[np.isin(X[feature], bin['bin']), new_feature] = bin['woe']
+                            X.loc[np.isin(X[feature], bin["bin"]), new_feature] = bin[
+                                "woe"
+                            ]
                         else:
-                            X.loc[np.logical_and(X[feature] >= np.min(bin['bin']),
-                                                 X[feature] <= np.max(bin['bin'])), new_feature] = bin['woe']
+                            X.loc[
+                                np.logical_and(
+                                    X[feature] >= np.min(bin["bin"]),
+                                    X[feature] <= np.max(bin["bin"]),
+                                ),
+                                new_feature,
+                            ] = bin["woe"]
 
         return X
 
+    def fit_transform(
+        self,
+        X: Union[pd.DataFrame, np.ndarray],
+        y: Union[pd.Series, np.ndarray],
+        hight_IV_threshold: float = 0.02,
+        suffix: str = "_WOE",
+    ):
+        self.fit(X=X, y=y)
+        X = self.transform(
+            X=X, y=y, hight_IV_threshold=hight_IV_threshold, suffix=suffix
+        )
 
-    def fit_transform(self,
-                      X: Union[pd.DataFrame, np.ndarray],
-                      y: Union[pd.Series, np.ndarray],
-                      hight_IV_threshold: float = 0.02,
-                      suffix: str = '_WOE'):
-        self.fit(X=X,
-                 y=y)
-        X = self.transform(X=X,
-                           y=y,
-                           hight_IV_threshold=hight_IV_threshold,
-                           suffix=suffix)
-        
         return X
-            
 
-    def _check_inputs(self,
-                      X: Union[pd.DataFrame, np.ndarray],
-                      y: Union[pd.DataFrame, np.ndarray]) -> Tuple[np.ndarray, np.ndarray]:
+    def _check_inputs(
+        self, X: Union[pd.DataFrame, np.ndarray], y: Union[pd.DataFrame, np.ndarray]
+    ) -> Tuple[np.ndarray, np.ndarray]:
         """
         Check input data
         :param X: data matrix
         :param y: target vector
         :return: X, y
         """
-        if type_of_target(y) != 'binary':
-            raise ValueError('y vector should be binary')
-        
-        X, y = check_X_y(X, y,
-                         accept_sparse=False,
-                         force_all_finite=False,
-                         dtype=None,
-                         y_numeric=True)
+        if type_of_target(y) != "binary":
+            raise ValueError("y vector should be binary")
+
+        X, y = check_X_y(
+            X,
+            y,
+            accept_sparse=False,
+            force_all_finite=False,
+            dtype=None,
+            y_numeric=True,
+        )
         return X, y
 
-
-    def _print(self,
-               msg: str):
+    def _print(self, msg: str):
         if self.verbose:
             print(msg)
