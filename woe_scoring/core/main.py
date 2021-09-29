a4cabf19449789487b5001e460fa1c37db314ab2
kiraplenkin <kiraplenkin@gmail.com>
2021-09-29 12:11:03 +0300

    Change create model class

--- woe_scoring/core/main.py
+++ woe_scoring/core/main.py
@@ -255,44 +255,16 @@ class WOETransformer(BaseEstimator, TransformerMixin):
             print(msg)
 
 
-def _calc_score(
-        x: pd.DataFrame,
-        y: Union[pd.Series, np.ndarray],
-        var: str,
-        random_state: int = None,
-        class_weight: str = None,
-        cv: int = 3,
-        c: float = None,
-        scoring: str = "roc_auc",
-        n_jobs: int = None,
-) -> float:
-    model = LogisticRegression(
-        random_state=random_state,
-        class_weight=class_weight,
-        n_jobs=n_jobs,
-        C=c,
-    )
-    scores = cross_val_score(
-        model,
-        x[var].values.reshape(-1, 1),
-        y,
-        cv=cv,
-        scoring=scoring,
-        n_jobs=n_jobs,
-    )
-    return (np.mean(scores) * 2 - 1) * 100
-
-
 class CreateModel(BaseEstimator, TransformerMixin):
     def __init__(
             self,
             max_vars: int = 20,
-            verbose: bool = False,
             special_cols: List = None,
             unused_cols: List = None,
             n_jobs: int = None,
             gini_threshold: float = 5.0,
             delta_train_test_threshold: float = 0.2,
+            corr_threshold: float = 0.5,
             random_state: int = None,
             class_weight: str = None,
             direction: str = "forward",
@@ -301,16 +273,15 @@ class CreateModel(BaseEstimator, TransformerMixin):
             scoring: str = "roc_auc",
             save_report: bool = True,
             path_to_save: str = os.getcwd(),
-            find_best_params: bool = False,
     ):
 
         self.max_vars = max_vars
-        self.verbose = verbose
-        self.special_cols = special_cols
-        self.unused_cols = unused_cols
+        self.special_cols = special_cols if special_cols else []
+        self.unused_cols = unused_cols if unused_cols else []
         self.n_jobs = n_jobs
         self.gini_threshold = gini_threshold
         self.delta_train_test_threshold = delta_train_test_threshold
+        self.corr_threshold = corr_threshold
         self.random_state = random_state
         self.class_weight = class_weight
         self.direction = direction
@@ -319,32 +290,60 @@ class CreateModel(BaseEstimator, TransformerMixin):
         self.scoring = scoring
         self.save_report = save_report
         self.path_to_save = path_to_save
-        self.find_best_params = find_best_params
 
-        self.feature_names = []
+        self.feature_names_: List[str] = []
+        self.coef_: List[float] = []
+        self.intercept_: float = 0.0
         self.model = None
+
+    def _calc_score(
+            self,
+            x: pd.DataFrame,
+            y: Union[pd.Series, np.ndarray],
+            var: str,
+            random_state: int = None,
+            class_weight: str = None,
+            cv: int = 3,
+            c: float = None,
+            scoring: str = "roc_auc",
+            n_jobs: int = None,
+    ) -> float:
+        model = LogisticRegression(
+            random_state=random_state,
+            class_weight=class_weight,
+            n_jobs=n_jobs,
+            C=c,
+        )
+        scores = cross_val_score(
+            model,
+            x[var].values.reshape(-1, 1),
+            y,
+            cv=cv,
+            scoring=scoring,
+            n_jobs=n_jobs,
+        )
+        return (np.mean(scores) * 2 - 1) * 100
 
     def fit(self, x: pd.DataFrame, y: Union[pd.Series, np.ndarray]):
         if isinstance(x, pd.DataFrame):
             x = x.drop(self.special_cols + self.unused_cols, axis=1)
-            # else:
-            #     x = x.drop(self.unused_cols, axis=1)
-            self.feature_names = x.columns
+            self.feature_names_ = x.columns
         elif isinstance(x, np.ndarray):
-            self.feature_names = [f"X_{i}" for i in range(x.shape[-1])]
+            self.feature_names_ = [f"X_{i}" for i in range(x.shape[-1])]
         else:
             raise TypeError("x vector is not np array neither data frame")
 
         if self.C is None:
             self.C = 1.0e4 / x.shape[0]
 
+        #  check features for min gini threshold
         to_drop = []
-        for i in range(len(self.feature_names)):
+        for i in range(len(self.feature_names_)):
             if (
-                    _calc_score(
+                    self._calc_score(
                         x,
                         y,
-                        self.feature_names[i],
+                        self.feature_names_[i],
                         self.random_state,
                         self.class_weight,
                         self.cv,
@@ -354,17 +353,18 @@ class CreateModel(BaseEstimator, TransformerMixin):
                     )
                     < self.gini_threshold
             ):
-                to_drop.append(self.feature_names[i])
+                to_drop.append(self.feature_names_[i])
 
-        self.feature_names = [var for var in self.feature_names if var not in to_drop]
+        self.feature_names_ = [var for var in self.feature_names_ if var not in to_drop]
 
+        #  check features for differance between train & test for min threshold
         to_drop = []
-        for i in range(len(self.feature_names)):
+        for i in range(len(self.feature_names_)):
             ceeds = np.random.randint(0, 100, self.cv)
             list_pct = []
             for ceed in ceeds:
                 temp_train_x, temp_test_x, temp_train_y, temp_test_y = train_test_split(
-                    x[self.feature_names[i]].values.reshape(-1, 1),
+                    x[self.feature_names_[i]].values.reshape(-1, 1),
                     y,
                     test_size=0.3,
                     stratify=y,
@@ -384,13 +384,14 @@ class CreateModel(BaseEstimator, TransformerMixin):
 
                 train_score = roc_auc_score(temp_train_y, y_prediction_train)
                 test_score = roc_auc_score(temp_test_y, y_prediction_test)
-                list_pct.append((train_score - test_score) / train_score)
+                list_pct.append(np.abs((train_score - test_score) / train_score))
             mean_pct = np.mean(list_pct)
             if mean_pct > self.delta_train_test_threshold:
-                to_drop.append(self.feature_names[i])
+                to_drop.append(self.feature_names_[i])
 
-        self.feature_names = [var for var in self.feature_names if var not in to_drop]
+        self.feature_names_ = [var for var in self.feature_names_ if var not in to_drop]
 
+        #  find selected features
         sfs = SequentialFeatureSelector(
             LogisticRegression(
                 random_state=self.random_state,
@@ -404,17 +405,18 @@ class CreateModel(BaseEstimator, TransformerMixin):
             n_jobs=self.n_jobs,
             scoring=self.scoring,
         )
-        sfs.fit(x[self.feature_names], y)
+        sfs.fit(x[self.feature_names_], y)
 
-        self.feature_names = list(np.array(self.feature_names)[list(sfs.get_support())])
+        self.feature_names_ = list(np.array(self.feature_names_)[list(sfs.get_support())])
 
-        for var_a in self.feature_names:
-            for var_b in self.feature_names:
+        #  check correlation
+        for var_a in self.feature_names_:
+            for var_b in self.feature_names_:
                 if (
                         var_a != var_b
-                        and abs(x[self.feature_names].corr()[var_a][var_b]) > 0.5
+                        and abs(x[self.feature_names_].corr()[var_a][var_b]) >= self.corr_threshold
                 ):
-                    if _calc_score(
+                    if self._calc_score(
                             x,
                             y,
                             var_a,
@@ -424,7 +426,7 @@ class CreateModel(BaseEstimator, TransformerMixin):
                             self.C,
                             self.scoring,
                             self.n_jobs,
-                    ) > _calc_score(
+                    ) > self._calc_score(
                         x,
                         y,
                         var_b,
@@ -435,69 +437,108 @@ class CreateModel(BaseEstimator, TransformerMixin):
                         self.scoring,
                         self.n_jobs,
                     ):
-                        self.feature_names.remove(var_b)
+                        self.feature_names_.remove(var_b)
                     else:
-                        self.feature_names.remove(var_a)
+                        self.feature_names_.remove(var_a)
                     break
 
-        temp_model = sm.Logit(y, sm.add_constant(x[self.feature_names])).fit()
+        self.model = sm.Logit(y, sm.add_constant(x[self.feature_names_])).fit()
 
+        #  check features for pvalue threshold
         retrain = False
-        for i, pvalue in enumerate(temp_model.wald_test_terms().table["pvalue"]):
+        for i, pvalue in enumerate(self.model.wald_test_terms().table["pvalue"]):
             if pvalue > 0.05:
-                self.feature_names.remove(temp_model.wald_test_terms().table.index[i])
-
+                self.feature_names_.remove(self.model.wald_test_terms().table.index[i])
+                retrain = True
         if retrain:
-            temp_model = sm.Logit(y, sm.add_constant(x[self.feature_names])).fit()
+            self.model = sm.Logit(y, sm.add_constant(x[self.feature_names_])).fit()
+
+        self.results = pd.read_html(self.model.summary().tables[1].as_html(), header=0, index_col=0)[0].reset_index()
+        self.intercept_ = self.results.iloc[0, 1]
+        self.coef_ = list(self.results.iloc[1:, 1])
+        self.feature_names_ = list(self.results.iloc[1:, 0])
 
         if self.save_report:
             try:
                 with open(
                         os.path.join(self.path_to_save, "model_summary.txt"), "w"
                 ) as outfile:
-                    outfile.write(temp_model.summary().as_text())
+                    outfile.write(self.model.summary().as_text())
 
                 with open(
                         os.path.join(self.path_to_save, "model_wald.txt"), "w"
                 ) as outfile:
-                    temp_model.wald_test_terms().summary_frame().to_string(outfile)
+                    self.model.wald_test_terms().summary_frame().to_string(outfile)
             except Exception as e:
                 print(f"Problem with saving: {e}")
 
-        self.model = LogisticRegression(
-            random_state=self.random_state,
-            class_weight=self.class_weight,
-            n_jobs=self.n_jobs,
-            C=self.C,
-        )
-        self.model.fit(x[self.feature_names], y)
-
-        return self
-
-    def predict(self, x: pd.DataFrame) -> np.ndarray:
+    def predict_proba(self, x: pd.DataFrame) -> np.ndarray:
         x = check_array(x)
-        prediction = self.model.predict(x)
+        prediction = self.model.predict(sm.add_constant(x))
         return prediction
 
-    def predict_proba(self, x: pd.DataFrame) -> List[float]:
-        x = check_array(x)
-        predict_proba = self.model.predict_proba(x)
-        return predict_proba
+    def generate_sql(self, encoder) -> str:
+        sql = list()
+        sql.append("with a as " + "(SELECT ")
+        sql.append(",".join([var.replace("WOE_", "") for var in self.feature_names_]))
+        sql.append("")
 
-    def fit_predict(
-            self, x: pd.DataFrame, y: Union[pd.Series, np.ndarray]
-    ) -> np.ndarray:
-        self.fit(x=x, y=y)
-        prediction = self.predict(x=x)
-        return prediction
+        for var in self.feature_names_:
+            for i, _ in enumerate(encoder.woe_iv_dict):
+                if list(encoder.woe_iv_dict[i])[0] == var.replace("WOE_", ""):
+                    if encoder.woe_iv_dict[i]["type_feature"] == "cat":
+                        sql.append(", CASE")
+                        for bin in encoder.woe_iv_dict[i][var.replace("WOE_", "")]:
+                            sql.append(
+                                f" WHEN {var.replace('WOE_', '')} in {bin['bin']} THEN {bin['woe']}".replace(
+                                    "[",
+                                    "("
+                                ).replace(
+                                    "]", ")"
+                                ).replace(", -1", "").replace(", Missing", "")
+                            )
+                        if encoder.woe_iv_dict[i]["missing_bin"] == "first":
+                            sql.append(
+                                f" WHEN {var.replace('WOE_', '')} IS NULL THEN {encoder.woe_iv_dict[i][var.replace('WOE_', '')][0]['woe']}"
+                            )
+                            sql.append(f" ELSE {encoder.woe_iv_dict[i][var.replace('WOE_', '')][0]['woe']}")
+                        elif encoder.woe_iv_dict[i]["missing_bin"] == "last":
+                            sql.append(
+                                f" WHEN {var.replace('WOE_', '')} IS NULL THEN {encoder.woe_iv_dict[i][var.replace('WOE_', '')][len(encoder.woe_iv_dict[i][var.replace('WOE_', '')]) - 1]['woe']}"
+                            )
+                            sql.append(
+                                f" ELSE {encoder.woe_iv_dict[i][var.replace('WOE_', '')][len(encoder.woe_iv_dict[i][var.replace('WOE_', '')]) - 1]['woe']}"
+                            )
+                        sql.append(f" END AS {var}")
+                    else:
+                        sql.append(", CASE")
+                        if encoder.woe_iv_dict[i]["missing_bin"] == "first":
+                            sql.append(
+                                f" WHEN {var.replace('WOE_', '')} IS NULL THEN {encoder.woe_iv_dict[i][var.replace('WOE_', '')][0]['woe']}"
+                            )
+                        elif encoder.woe_iv_dict[i]["missing_bin"] == "last":
+                            sql.append(
+                                f" WHEN {var.replace('WOE_', '')} IS NULL THEN {encoder.woe_iv_dict[i][var.replace('WOE_', '')][len(encoder.woe_iv_dict[i][var.replace('WOE_', '')]) - 1]['woe']}"
+                            )
+                        for n, bin in enumerate(encoder.woe_iv_dict[i][var.replace("WOE_", "")]):
+                            if n == 0:
+                                sql.append(f" WHEN {var.replace('WOE_', '')} < {bin['bin'][1]} THEN {bin['woe']}")
+                            elif n == len(encoder.woe_iv_dict[i][var.replace("WOE_", "")]) - 1:
+                                sql.append(f" WHEN {var.replace('WOE_', '')} >= {bin['bin'][0]} THEN {bin['woe']}")
+                            else:
+                                sql.append(
+                                    f" WHEN {var.replace('WOE_', '')} >= {bin['bin'][0]} AND {var.replace('WOE_', '')} < {bin['bin'][1]} THEN {bin['woe']}"
+                                )
+                        sql.append(f" END AS {var}")
 
-    def fit_predict_proba(
-            self, x: pd.DataFrame, y: Union[pd.Series, np.ndarray]
-    ) -> List[float]:
-        self.fit(x=x, y=y)
-        predict_proba = self.predict_proba(x)
-        return predict_proba
+        sql.append(f" FROM )")
+        sql.append(", b as (")
+        sql.append("SELECT a.*")
+        sql.append(f", REPLACE(1 / (1 + EXP(-({self.results.iloc[0, 1]}")
+        for idx in range(1, self.results.shape[0]):
+            sql.append(f" + ({self.results.iloc[idx, 1]} * a.{self.results.iloc[idx, 0]})")
+        sql.append("))), ',', '.') as PD")
+        sql.append(" FROM a) ")
+        sql.append("SELECT * FROM b")
 
-    def _print(self, msg: str):
-        if self.verbose:
-            print(msg)
+        return "".join(sql)
